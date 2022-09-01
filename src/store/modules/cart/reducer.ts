856746f6aef38e9be7797e079d4749df14779be5
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-09-01 14:14:54 -0300

    .

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -29,35 +29,3 @@ import {
   export const cartStateData = (state: ICartState) => state.cart.cart;
   
   export default CartData.reducer;
-
-
-// import {
-//     createSlice,
-//     PayloadAction
-// } from '@reduxjs/toolkit';
-
-// import { 
-//     ICartState, 
-//     ICart,
-//     IProduct
-// } from '../../../types';
-
-// const initialState = {
-//     cart: []
-// }
-
-// const CartData = createSlice({
-//     name: 'cartData',
-//     initialState,
-//     reducers: {
-//         addNewItem: ( state: ICart, action: PayloadAction<IProduct> ) => {
-//             state.cart = [ ...state.cart, action.payload]
-//         }
-//     }
-// });
-
-// export const { addNewItem } = CartData.actions;
-
-// export const cartStateData = (state: ICartState) => state.cartState.cart;
-
-// export default CartData.reducer;
