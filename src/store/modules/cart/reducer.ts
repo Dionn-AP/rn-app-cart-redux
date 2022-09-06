7370dc1032fa35f82b49cba144a028bfd894fec4
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-09-06 15:06:00 -0300

    configuração do reducer

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,30 +1,50 @@
 import {
-    createSlice,
-    PayloadAction
-  } from '@reduxjs/toolkit';
-  
-  import { 
-    ICart,
-    IProduct,
-    ICartState
-  } from '../../../types/index'
-  
-  const initialState = {
-    cart: []
-  }
-  
-  const CartData = createSlice({
-    name: 'cartData',
-    initialState,
-    reducers: {
-      addNewItem: ( state: ICart, action) => {
-        state.cart = [...state.cart, action.payload]
+  createSlice,
+  PayloadAction
+} from '@reduxjs/toolkit';
+
+import {
+  ICart,
+  IProduct,
+  ICartState
+} from '../../../types/index'
+
+const initialState = {
+  cart: [],
+}
+
+const CartData = createSlice({
+  name: 'cartData',
+  initialState,
+  reducers: {
+    addNewItem: (state: ICart, action) => {
+      if (state.cart.find(item => item.id === action.payload.id)) {
+        let index = state.cart.findIndex(item => item.id === action.payload.id);
+        state.cart[index].amount += 1
+        return
+      }
+      state.cart = [...state.cart, action.payload];
+    },
+
+    removeItems: (state: ICart, action) => {
+      if (state.cart.find(item => item.id === action.payload.id)) {
+        let index = state.cart.findIndex(item => item.id === action.payload.id);
+        if (state.cart[index].amount > 1) {
+          state.cart[index].amount -= 1
+          return
+        }
+        if (state.cart[index].amount === 1) {
+          state.cart[index].amount -= 1
+          state.cart.splice(index, 1);
+          return
+        }
       }
     }
-  })
-  
-  export const { addNewItem } = CartData.actions;
-  
-  export const cartStateData = (state: ICartState) => state.cart.cart
-  
-  export default CartData.reducer;
+  }
+});
+
+export const { addNewItem, removeItems } = CartData.actions;
+
+export const cartStateData = (state: ICartState) => state.cart.cart
+
+export default CartData.reducer;
