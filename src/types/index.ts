051bb8f86ec0a29ae758e553596d1888e836f3cd
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-09-06 15:06:08 -0300

    types e interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,18 +1,22 @@
 export interface IProduct {
-    id: number;
-    name: string;
-    brand: string;
-    image: string;
-    price: number;
-    inStock: boolean;
+  id: number;
+  name: string;
+  brand: string;
+  image: string;
+  price: number;
+  inStock: boolean;
+  amount: number;
+  camera: string;
+  screen: string;
+  storage: string;
 }
 
 export interface ICart {
-    cart: IProduct[]
+  cart: IProduct[]
 }
 
 export interface ICartState {
-    cart: {
-      cart: IProduct[]
-    }
+  cart: {
+    cart: IProduct[]
   }
+}
